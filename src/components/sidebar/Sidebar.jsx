59f0e72c810af5417508242e430e9d62efad3ec4
Almaz <almaz.esengulov.pi9.03.20@gmail.com>
2023-02-20 12:53:01 +0600

    create Sidebar and Layout

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { Components } from '..';
+
+import cls from "../../assets/styles/sidebar/Sidebar.module.scss";
+import Logo from "../../assets/images/logo/insta.png"
+
+
+export default function Sidebar() {
+    return (
+        <section className={cls.sidebar_wrapper}>
+            <div className={cls.sidebar_wrapper_content}>
+                <Components.Image src={Logo} />
+            </div>
+        </section>
+    )
+};
--- src/pages/layout/LayoutRoutes.jsx
+++ src/pages/layout/LayoutRoutes.jsx
@@ -1,16 +1,13 @@
 
 
 import React from 'react'
-import { Route, Routes as Switch } from 'react-router-dom'
-import { Layout } from '../../services/Path'
-import { LayoutPages } from '../Lazy'
+import Layouts from './Layout'
+
 
 export default function LayoutRoutes() {
     return (
         <React.Fragment>
-            <Switch>
-                <Route path={Layout.home} element={<LayoutPages.Home />} />
-            </Switch>
+            <Layouts />
         </React.Fragment>
     )
 }
